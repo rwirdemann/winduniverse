35e8cb96bb01e9135ef822a3b0d495631f8352cd
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2023-04-21 07:59:45 +0200

    Adds session and spot model

--- windcockpit/models.py
+++ windcockpit/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Spot(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Session(models.Model):
+    sport = models.CharField(max_length=200)
+    date = models.DateTimeField("date of session")
+    spot = models.ForeignKey(Spot, on_delete=models.RESTRICT)
+
+    # distance in meter
+    distance = models.FloatField
+
+    # max speed in m/s
+    max_speed = models.FloatField
+
+    # duration in minutes
+    duration = models.IntegerField
+
+    def __str__(self):
+        return f'{self.sport} {self.date}'
--- winduniverse/settings.py
+++ winduniverse/settings.py
@@ -27,12 +27,13 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['95.217.180.178']
+ALLOWED_HOSTS = ['95.217.180.178', '127.0.0.1']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'windcockpit.apps.WindcockpitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
