c3d701bc52fc901c4cc3662674d2cd27c3926a8f
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2023-04-22 14:50:29 +0200

    Changes base url to wp

--- winduniverse/urls.py
+++ winduniverse/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("windcockpit/", include("windcockpit.urls")),
+    path("wp/", include("windcockpit.urls")),
     path('admin/', admin.site.urls),
 ]
