0a98ca765985dcf4b22f0de7c307ed7c99662a0e
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2023-05-03 21:32:52 +0200

    Adds datepicker

--- windcockpit/forms.py
+++ windcockpit/forms.py
@@ -1,9 +1,13 @@
 from django.forms import ModelForm
 
 from windcockpit.models import Session
+from windcockpit.widgets import DatePickerInput
 
 
 class SessionForm(ModelForm):
     class Meta:
         model = Session
-        fields = ["sport", "spot"]
+        fields = ["sport", "spot", "date"]
+        widgets = {
+            "date": DatePickerInput()
+        }
